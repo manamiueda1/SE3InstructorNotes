0ba05e808f9a86e0bb7a624790c8488d121f4b29
Manami Ueda <manamiueda1@gmail.com>
2020-10-06 15:23:04 -0400

    organized notes for DataTypeNotes.js

--- JavaScript/DataTypeNotes.js
+++ JavaScript/DataTypeNotes.js
@@ -1,3 +1,23 @@
+// VARIABLE NOTES
+
+// What are variables?
+
+// Types of variables
+
+// console.log()
+
+// DATA TYPE NOTES
+
+// What are Data Types?
+
+//  Differenet Data Types
+
 // Primitive Data Types:
 
 // Non-Primitave Data Types
+
+// Undefined vs null vs not defined
+
+// Mutable vs non-mutable
+
+// Complex types
