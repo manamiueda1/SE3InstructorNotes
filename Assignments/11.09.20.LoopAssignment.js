8e575bbe7f69680b6f7d840a91c9b2e96d3354c9
Manami Ueda <manamiueda1@gmail.com>
2020-11-09 19:54:24 -0500

    went over loops and conditional assignment problems and added practice problems

--- Assignments/11.09.20.LoopAssignment.js
+++ Assignments/11.09.20.LoopAssignment.js
@@ -2,17 +2,122 @@
 // Log every character in this string 
 // Once all characters are successfully logged, return the string, 'Done!'
 
+// function functionLoops(tauseef){
+//     for(let i = 0; i <= tauseef.length - 1; i++){
+//         console.log(tauseef[i])
+//     }
+//     return 'Done!' 
+// }
+
+// console.log(functionLoops('string'));
+// s
+// t
+// r
+// i
+// n
+// g
+// Done!
+
 // Create a function that takes in a number as a parameter. 
 // This function will log from 1- any number passed in this function as an argument
 // After all numbers are successfully logged, return the string, 'Done!'
+
+// function speedLimit (num1){
+//     for (let i = 1; i <= num1; i++){
+//         console.log(i)
+//     }
+//     return 'Done!'
+// }
+// console.log(speedLimit(6))
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+// // 6
+// // Done!
+// console.log(speedLimit(18))
+// console.log(speedLimit(10))
+
 
 // Create a function that takes in an array as a parameter. 
 // This function will Loop/iterate through the array and log every element inside the array
 // After all elements are logged, return the string, 'Done!'
 
+// let array = ['apple', 'orange', 'banana', 'pear'];
+
+// function iterateChar(stringArray){
+//     for (let i = 0; i < stringArray.length; i++){
+//         console.log(stringArray[i])
+//     }
+//     return 'Done!'
+// }
+// console.log(iterateChar(array))
+
+// apple
+// orange
+// banana
+// pear
+// Done!
+
+
 // Create a function that takes in a string. 
 // This function will loop through the string and log the indices of each character in the string.
 // After the indices are logged, return the string, 'Done!'
 
+// let snowball = 'manifest'
+
+// function doorKnob(indication){
+//     for (let i = 0; i < indication.length; i++){
+//         console.log(i)
+//     }
+//     return 'Done!'
+// } 
+// console.log(doorKnob(snowball))
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// Done!
+
 // Create a function that only logs the even numbers from 1-10
 // After the even numbers are logged, return the string, 'Done!'
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// function evenNumbers (numbers){
+//     for (let i = 1; i < numbers.length; i+=2){
+//         console.log(numbers[i])
+//     }
+//     return 'Done!'
+// }
+// console.log(evenNumbers(numbers))
+
+// 2
+// 4
+// 6
+// 8
+// 10
+// Done!
+
+// function evenNum (){
+//     for(let i = 1; i <= 10; i++){
+//         if (i % 2 === 0){
+//             console.log(i)
+//         }
+//     }
+//     return 'Done!'
+// }
+// console.log(evenNum())
+
+
+// 2
+// 4
+// 6
+// 8
+// 10
+// Done!
--- JavaScript/LoopConditionalNotes.js
+++ JavaScript/LoopConditionalNotes.js
@@ -1,18 +1,32 @@
 //  COMBINING LOOPS AND CONDITIONALS 
 
-let numArray = [12, 3, 5, 12, 5, 23, 8, 4] 
+// let numArray = [12, 3, 5, 12, 5, 23, 8, 4] 
 
-// if the number is greater than 20 log it to the terminal 
+// // if the number is greater than 20 log it to the terminal 
 
-function onlyGreaterThanTwenty (arr){
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] > 20){
-            console.log(arr[i])
-        } else {
-            console.log('number does not satisfy')
-        }
-    }
-    return 'is it working'
-}
-console.log(onlyGreaterThanTwenty(numArray))
+// function onlyGreaterThanTwenty (arr){
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] > 20){
+//             console.log(arr[i])
+//         } else {
+//             console.log('number does not satisfy')
+//         }
+//     }
+//     return 'is it working'
+// }
+// console.log(onlyGreaterThanTwenty(numArray))
+
+/* -------------------------------------------------------------------------- */
+
+// PRACTICE PROBLEMS 
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// add all of the numbers in the array
+
+// add only the odd numbers in the array
+
+// count how many numbers are even in the array
+
+// add the numbers that are NOT divisible by 3
 
