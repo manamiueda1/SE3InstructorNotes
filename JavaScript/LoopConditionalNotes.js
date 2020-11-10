c35afc4425a2c5b816460885a06fad4161b65d97
Manami Ueda <manamiueda1@gmail.com>
2020-11-09 20:56:24 -0500

    added loop conditional practice problems

--- JavaScript/LoopConditionalNotes.js
+++ JavaScript/LoopConditionalNotes.js
@@ -20,13 +20,60 @@
 
 // PRACTICE PROBLEMS 
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 // add all of the numbers in the array
 
+// function addNums (numArr){
+//     let total = 0;
+
+//     for (let i = 0; i < numArr.length; i++){
+//         total += numArr[i]
+//     }
+//     return total 
+// }
+// console.log(addNums(array)) //45
+
 // add only the odd numbers in the array
+
+// function onlyAddOdds (oddArr){
+//     let total = 0
+//     for (let i = 0; i < oddArr.length; i++){
+//         if (oddArr[i] % 2 === 1){
+//             total += oddArr[i]
+//         }
+//     }
+//     return total
+// }
+// console.log(onlyAddOdds(array)) // 25
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // count how many numbers are even in the array
 
+function onlyEvens (evenArr){
+    let count = 0
+
+    for (let i = 0; i < evenArr.length; i++){
+        if (evenArr[i] % 2 === 0){
+            count += 1
+        }
+    }
+    return count
+}
+console.log(onlyEvens(array)) // 4
+
 // add the numbers that are NOT divisible by 3
+
+function divThree (divArr){
+    let total = 0
+    for (let i = 0; i < divArr.length; i++){
+        if (divArr[i] % 3 === 0){
+            continue
+        }
+        total += divArr[i]
+    }
+    return total 
+}
+console.log(divThree(array)) // 27
 
