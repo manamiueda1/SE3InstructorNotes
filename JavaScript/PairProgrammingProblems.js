8afd353f02d1bf975446634dfc80c78eb5350376
Manami Ueda <manamiueda1@gmail.com>
2020-12-28 16:02:28 -0500

    added pair programming problems

---
+++ JavaScript/PairProgrammingProblems.js
@@ -0,0 +1,9 @@
+// Pair Programming Problems
+
+// 12.28.20
+
+// Expressions matter
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e
+
+// Squash the bugs
+// https://www.codewars.com/kata/56f173a35b91399a05000cb7/train/javascript
