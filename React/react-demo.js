c8fe29025129bfca81214b459fba34b35868b2cb
Manami Ueda <manamiueda1@gmail.com>
2021-02-17 20:47:31 -0500

    adding data for demo

---
+++ React/react-demo.js
@@ -0,0 +1,44 @@
+let studentData = [
+  {
+    id: 1,
+    name: "Eddy",
+    color: "chartreuse",
+    school: "Nebula Academy",
+  },
+  {
+    id: 2,
+    name: "Chelsea",
+    color: "purple",
+    school: "Nebula Academy",
+  },
+  {
+    id: 3,
+    name: "Tauseef",
+    color: "orange",
+    school: "Nebula Academy",
+  },
+  {
+    id: 4,
+    name: "Maurice",
+    color: "red",
+    school: "Nebula Academy",
+  },
+  {
+    id: 5,
+    name: "D'aja",
+    color: "blue",
+    school: "Nebula Academy",
+  },
+  {
+    id: 6,
+    name: "Jasmine",
+    color: "pink",
+    school: "Nebula Academy",
+  },
+  {
+    id: 7,
+    name: "Anitria",
+    color: "blue",
+    school: "Nebula Academy",
+  },
+];
